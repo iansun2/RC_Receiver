af04f07bd8c71b18233ba3bcb99111de26a7f66e
iansun2 <iansun2004@gmail.com>
2021-02-07 16:42:16 +0800

    change RF24 library

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 #include <Arduino.h>
+#include <nRF24L01.h>
+#include <printf.h>
+#include <RF24.h>
+#include <RF24_config.h>
+
+#define CE_pin 6
+#define CSN_pin 7
+#define R_EN 2
+#define L_EN 3
+#define R_PWM 9
+#define L_PWM 10
+
+
 
 void setup() {
-  // put your setup code here, to run once:
+  TCCR1B |= (1 << CS11);
 }
 
 void loop() {
